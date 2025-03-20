958fa4c0d06d0c4c2f50171682c2fcd25ec96c04
Akram Louze <akramlouze2005@gmail.com>
2025-03-20 21:08:34 +0100

    Added build script with Java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,18 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.36")
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.akraml.Main"
+    }
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
+
 tasks.test {
     useJUnitPlatform()
 }
